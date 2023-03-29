fb47fd7f5400ae68cc2c73ec51c0fd16978ffa25
cameron <hammerbro_97@hotmail.com>
2023-03-28 19:05:09 -0600

    made cookies

---
+++ js/app.js
@@ -0,0 +1,46 @@
+function change_the_image(image){
+
+if(image[`target`].getAttribute(`src`) === `/images/399.png`){
+
+    image[`target`].setAttribute(`src`, `https://www.serebii.net/swordshield/pokemon/399.png`);
+
+}else{
+
+    image[`target`].setAttribute(`src`, `/images/399.png`);
+}
+
+};
+
+let change_image = document.querySelectorAll(`.page_span`);
+
+for(counter = 0; counter < change_image.length; counter = counter +1){
+
+change_image[counter].addEventListener(`click`, change_the_image)
+
+
+};
+
+
+
+function button_clicker(){
+
+    Cookies.set(`button_clicked`, `yes`);
+};
+
+let cookies_getter = document.querySelector(`#clickme`);
+cookies_getter.addEventListener(`click`, button_clicker);
+
+let clicked = Cookies.get(`button_clicked`);
+
+if (clicked === undefined){
+
+cookies_getter.insertAdjacentHTML(`afterend`, `<h1>please click the button</h1>`);
+
+}else{
+
+    cookies_getter.insertAdjacentHTML(`afterend`, `<h1>Good job making a cookie</h1>`);
+}
+
+
+
+
